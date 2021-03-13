38c6fa489cd7018c56ae877c7f5a255bfd7f08b5
Chiro <sachinsinghroxx@gmail.com>
2021-03-13 12:56:39 +0530

    did basic queries

--- website/app.js
+++ website/app.js
@@ -1,3 +1,51 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('bodyParser');
+
+
+
+const sqlite3 = require('sqlite3').verbose();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+app.use(bodyParser.json());
+
+let db = new sqlite3.Database('../database/test.db',(err)=>{
+    if(err){
+        return console.error(err.message);
+    }
+    console.log('====================================');
+    console.log("connected to in temporary database sqlite");
+    console.log('====================================');
+});
+
+
+
+
+db.serialize(()=>{
+    db.each(`SELECT item_id as id,item_name as name,item_description as description FROM ITEMS`,(err,row)=>{
+        if(err){
+            console.error(err.message);
+        }
+        console.log(row.id + '\t\t' + row.name +'\t\t' + row.description);
+    })
+})
+
+db.serialize(()=>{
+    db.each(`SELECT user_id as id,user_name as name,valid_user as valid FROM USERS`,(err,row)=>{
+        if(err){
+            console.error(err.message);
+        }
+        console.log(row.id + '\t\t' + row.name +'\t\t' + row.valid);
+    })
+})
+
+db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
