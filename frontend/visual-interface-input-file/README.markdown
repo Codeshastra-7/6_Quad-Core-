255bae3f0fd913f4d88f1fa5e809230a6b0cc7e2
pooja <poojawani.0304@gmail.com>
2021-03-13 18:58:37 +0530

    added upload photos form

---
+++ frontend/visual-interface-input-file/README.markdown
@@ -0,0 +1,5 @@
+# Visual interface input file
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/brunodup/pen/zrBVwW](https://codepen.io/brunodup/pen/zrBVwW).
+
+Visual design for a upload photo in a form
---
+++ frontend/visual-interface-input-file/src/script.js
@@ -0,0 +1,20 @@
+
+var inputAddphoto = '<div class="upload-photo">Foto de perfil</div>',
+    inputphoto = $('#id_photo');
+
+inputphoto.before(inputAddphoto);
+
+$('.upload-photo').on('click', function() {
+    $(this).siblings('#id_photo').trigger('click');
+});
+
+inputphoto.on('change', function(){
+    var input = $(this),
+        reader = new FileReader();
+
+    reader.onload = function (e) {
+        input.siblings('.upload-photo').css('background-image', 'url(' + e.target.result + ')');
+    };
+
+    reader.readAsDataURL(this.files[0]);
+});
