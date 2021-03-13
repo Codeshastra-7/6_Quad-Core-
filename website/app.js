c6c4bc3f28c6bb49b639072ab01a68686fd0cb59
Chiro <sachinsinghroxx@gmail.com>
2021-03-13 13:35:21 +0530

    updated rendering

--- website/app.js
+++ website/app.js
@@ -1,51 +1,105 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const ejs = require("ejs");
+// const json = require("json");
+const bodyParser = require("body-parser");
 
-const bodyParser = require('bodyParser');
 
+const sqlite3 = require("sqlite3").verbose();
 
-
-const sqlite3 = require('sqlite3').verbose();
-
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(bodyParser.json());
 
-let db = new sqlite3.Database('../database/test.db',(err)=>{
-    if(err){
-        return console.error(err.message);
-    }
-    console.log('====================================');
-    console.log("connected to in temporary database sqlite");
-    console.log('====================================');
+
+app.set('view engine', 'ejs')
+
+
+let db = new sqlite3.Database("../database/test.db", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("====================================");
+  console.log("connected to in temporary database sqlite");
+  console.log("====================================");
 });
 
+// db.serialize(() => {
+//   db.each(
+//     `SELECT item_id as id,item_name as name,item_description as description FROM ITEMS`,
+//     (err, row) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//       console.log(row.id + "\t\t" + row.name + "\t\t" + row.description);
+//     }
+//   );
+// });
 
+// db.serialize(() => {
+//   db.each(
+//     `SELECT user_id as id,user_name as name,valid_user as valid FROM USERS`,
+//     (err, row) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//       console.log(row.id + "\t\t" + row.name + "\t\t" + row.valid);
+//     }
+//   );
+// });
 
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log("Close the database connection.");
+// });
 
-db.serialize(()=>{
-    db.each(`SELECT item_id as id,item_name as name,item_description as description FROM ITEMS`,(err,row)=>{
+app.get('/', (req, res) => {
+    var data=null;
+    var sql = "select item_id as id,item_id as id,item_id as item_de from ITEMS"
+    var params=[]
+    db.all(sql,params,(err,rows) => {
         if(err){
-            console.error(err.message);
+            res.status(400).json({"error":err.message});
+            return;
         }
-        console.log(row.id + '\t\t' + row.name +'\t\t' + row.description);
+        console.log(rows);
+        res.render('homepage',{data:
+            rows
+        });
     })
+    res.render('homepage',{data:{}});
 })
 
-db.serialize(()=>{
-    db.each(`SELECT user_id as id,user_name as name,valid_user as valid FROM USERS`,(err,row)=>{
+app.get('/getData', (req, res) => {
+    var data=null;
+    var sql = "select * from USERS"
+    var params=[]
+    db.all(sql,params,(err,rows) => {
         if(err){
-            console.error(err.message);
+            res.status(400).json({"error":err.message});
+            return;
         }
-        console.log(row.id + '\t\t' + row.name +'\t\t' + row.valid);
+        console.log(rows);
+        res.render('homepage',{data:
+            rows
+        });
+        // data = json({
+        //     "message":"success",
+        //     "data":rows
+        // })
     })
+    // console.log(data);
+    // res.render('homepage',{data:data.data});
 })
 
-db.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
+app.listen(3000,()=>{
+    console.log('====================================');
+    console.log("listing on port 3000");
+    console.log('====================================');
+})
